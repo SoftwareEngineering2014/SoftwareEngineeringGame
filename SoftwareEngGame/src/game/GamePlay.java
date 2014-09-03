4316e9c954735fc912e2244c09f577e32faafbf3
DarkVortex <arnav98@gmail.com>
2014-09-03 10:19:47 -0400

    Finished the renaming of players in their objects

--- SoftwareEngGame/src/game/GamePlay.java
+++ SoftwareEngGame/src/game/GamePlay.java
@@ -5,14 +5,19 @@ import java.util.ArrayList;
 public class GamePlay {
 
 	public static ArrayList<String> playerNames = new ArrayList<String>();
-	
+	public static Player players[];
 	
 	public static void main(String [] args){
 		PlayerIO.getPlayers();
+		players = new Player[playerNames.size()];
+		for(int i = 0; i < playerNames.size(); i++){
+			players[i] = new Player(playerNames.get(i));
+		}
+		lineSep();
 	}
 	
-	public static void greetPlayer(Player p){
-		String out = String.format("<Console> Hello there, %s!", p.getName());
-		System.out.println(out);
+	public static void lineSep(){
+		String br = "------------------------------------------------------";
+		System.out.println(br);
 	}
 }
